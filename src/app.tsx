73392d5571ce024d906e81b496197ae831498cc0
Pedro Henrique Hoffmann da Cunha <phhcpedrohhcunha@gmail.com>
2024-07-09 14:14:23 -0300

    feat: add initial page

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,64 @@
+import { MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2 } from "lucide-react"
+import { useState } from "react"
+
 export function App() {
+  const [isGuestsInputOpen, setIsGuestsInputOpen] = useState<boolean>(false)
+
+  const openGuestsInput = () => {
+    setIsGuestsInputOpen(true);
+  };
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
+      <div className="max-w-3xl w-full px-6 text-center space-y-10">
+        <div className="flex flex-col items-center gap-3">
+          <img src="/logo.svg" alt="Plann.er" />
+          <p className="zinc-300 text-lg">
+            Convide seus amigos e planeje sua próxima viagem!
+          </p>
+        </div>
+        <div className="space-y-4">
+          <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
+            <div className="flex flex-1 items-center gap-2">
+              <MapPin size={20} className="text-zinc-400" />
+              <input type="text" placeholder="Para onde você vai?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
+            </div>
+            <div className="flex items-center gap-2">
+              <Calendar size={20} className="text-zinc-400" />
+              <input type="text" placeholder="Quando?" className="bg-transparent text-lg placeholder-zinc-400 outline-none w-40" />
+            </div>
+            <div className="w-px h-6 bg-zinc-800" />
+            {isGuestsInputOpen ? (
+              <button onClick={openGuestsInput} className="bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700">
+                Alterar local/data
+                <Settings2 size={20} />
+              </button>
+            ) : (
+              <button onClick={openGuestsInput} className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+                Adicionar amigos
+                <ArrowRight size={20} />
+              </button>
+            )}
+          </div>
+          {isGuestsInputOpen && (
+            <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
+              <div className="flex flex-1 items-center gap-2">
+                <UserRoundPlus size={20} className="text-zinc-400" />
+                <input type="text" placeholder="Quem estará na viagem?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
+              </div>
+              <div className="w-px h-6 bg-zinc-800" />
+              <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+                Confirmar viagem
+                <ArrowRight size={20} />
+              </button>
+            </div>
+          )}
+        </div>
+        <p className="text-sm text-zinc-500">
+          Ao planejar sua viagem pela plann.er você automaticamente concorda <br />
+          com nossos <a href="#" className="text-zinc-300 underline">termos de uso</a> e <a href="#" className="text-zinc-300 underline">política de privacidade</a>.
+        </p>
+      </div>
+    </div>
   )
 }
