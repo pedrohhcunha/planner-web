f720b24816f51031155128c7501d0e0494c610f5
Pedro Henrique Hoffmann da Cunha <phhcpedrohhcunha@gmail.com>
2024-07-09 20:10:22 -0300

    feat: add confirm trip modal and functionality to create a trip

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,10 @@
-import { MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2, X, AtSign, Plus } from "lucide-react"
+import { MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2, X, AtSign, Plus, User } from "lucide-react"
 import { useState, type FormEvent } from "react"
 
 export function App() {
   const [isGuestsInputOpen, setIsGuestsInputOpen] = useState<boolean>(false)
   const [isGuestsModalOpen, setIsGuestsModalOpen] = useState<boolean>(false)
+  const [isConfirmTripModalOpen, setIsConfirmTripModalOpen] = useState<boolean>(false)
 
   const [emailsToInvite, setEmailsToInvite] = useState<string[]>([]);
 
@@ -27,6 +28,14 @@ export function App() {
     setIsGuestsModalOpen(false);
   };
 
+  const openConfirmTripModal = () => {
+    setIsConfirmTripModalOpen(true);
+  }
+
+  const closeConfirmTripModal = () => {
+    setIsConfirmTripModalOpen(false);
+  }
+
   const addEmailToInvite = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
@@ -41,6 +50,21 @@ export function App() {
     setEmailsToInvite([...emailsToInvite, email]);
 
     event.currentTarget.reset();
+  };
+
+  const confirmTrip = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+
+    const name = formData.get('name')?.toString().trim();
+    const email = formData.get('email')?.toString().trim();
+
+    if (!name || !email) return;
+
+    console.log({ name, email });
+
+    closeConfirmTripModal();
   };
 
   return (
@@ -84,7 +108,7 @@ export function App() {
                 </span>
               </button>
               <div className="w-px h-6 bg-zinc-800" />
-              <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+              <button onClick={openConfirmTripModal} className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
                 Confirmar viagem
                 <ArrowRight size={20} />
               </button>
@@ -139,6 +163,46 @@ export function App() {
               <button type="submit" className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium hover:bg-lime-400 flex items-center gap-2">
                 Convidar
                 <Plus size={20} />
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
+      {isConfirmTripModalOpen && (
+        <div className="fixed inset-0 bg-black bg-opacity-60 flex items-center justify-center">
+          <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 flex flex-col gap-5">
+            <div className="flex flex-col gap-2">
+              <div className="flex justify-between items-center">
+                <h2 className="text-lg font-semibold">Confirmar criação de viagem</h2>
+                <button onClick={closeConfirmTripModal} className="text-zinc-400 hover:text-zinc-300">
+                  <X size={24} />
+                </button>
+              </div>
+              <p className="text-zinc-500 text-sm">
+                Para concluir a criação da viagem para <span className="text-zinc-100 font-semibold">Florianópolis, Brasil</span> nas datas de <span className="text-zinc-100 font-semibold">16 a 27 de Agosto de 2024</span> preencha seus dados abaixo:
+              </p>
+            </div>
+            <form onSubmit={confirmTrip} className="flex flex-col gap-3">
+              <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+                <User size={20} className="text-zinc-400" />
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Seu nome completo"
+                  className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+                />
+              </div>
+              <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+                <AtSign size={20} className="text-zinc-400" />
+                <input
+                  type="text"
+                  name="email"
+                  placeholder="Seu email pessoal"
+                  className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+                />
+              </div>
+              <button type="submit" className="w-full h-11 justify-center bg-lime-300 text-lime-950 rounded-lg px-5 font-medium hover:bg-lime-400 flex items-center gap-2">
+                Confirmar criação da viagem
               </button>
             </form>
           </div>
