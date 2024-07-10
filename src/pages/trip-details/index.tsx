10c10205199eba6a5c6c585ed23aaa1b2515fc1f
Pedro Henrique Hoffmann da Cunha <phhcpedrohhcunha@gmail.com>
2024-07-10 15:41:52 -0300

    chore: Update TripDetailsPage component to display trip details, activities, links, and guests

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,6 +1,12 @@
-import { Calendar, CircleCheck, CircleDashed, Link2, MapPin, Plus, Settings2, UserCog } from "lucide-react";
+import { AtSign, Calendar, CircleCheck, CircleDashed, Link2, MapPin, Plus, Settings2, Tag, User, UserCog, X } from "lucide-react";
+import { useState } from "react";
 
 export function TripDetailsPage() {
+  const [isCreateActivityModalOpen, setIsCreateActivityModalOpen] = useState<boolean>(false);
+
+  const openActivityModal = () => setIsCreateActivityModalOpen(true);
+  const closeActivityModal = () => setIsCreateActivityModalOpen(false);
+
   return (
     <div className="max-w-6xl px-6 py-10 mx-auto flex flex-col gap-8">
       <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between gap-2">
@@ -24,7 +30,7 @@ export function TripDetailsPage() {
         <div className="flex flex-1 flex-col gap-6">
           <div className="flex items-center justify-between">
             <h2 className="text-3xl font-semibold">Atividades</h2>
-            <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+            <button onClick={openActivityModal} className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
               <Plus size={20} />
               Adicionar atividade
             </button>
@@ -135,6 +141,46 @@ export function TripDetailsPage() {
           </div>
         </div>
       </main>
+      {isCreateActivityModalOpen && (
+        <div className="fixed inset-0 bg-black bg-opacity-60 flex items-center justify-center">
+          <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 flex flex-col gap-5">
+            <div className="flex flex-col gap-2">
+              <div className="flex justify-between items-center">
+                <h2 className="text-lg font-semibold">Confirmar criação de viagem</h2>
+                <button onClick={closeActivityModal} className="text-zinc-400 hover:text-zinc-300">
+                  <X size={24} />
+                </button>
+              </div>
+              <p className="text-zinc-500 text-sm">
+                Todos convidados podem visualizar as atividades.
+              </p>
+            </div>
+            <form className="flex flex-col gap-3">
+              <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+                <Tag size={20} className="text-zinc-400" />
+                <input
+                  type="text"
+                  name="title"
+                  placeholder="Qual a atividade?"
+                  className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+                />
+              </div>
+              <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+                <Calendar size={20} className="text-zinc-400" />
+                <input
+                  type="datetime-local"
+                  name="occurs_at"
+                  placeholder="Data e horário da atividade"
+                  className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+                />
+              </div>
+              <button type="submit" className="w-full h-11 justify-center bg-lime-300 text-lime-950 rounded-lg px-5 font-medium hover:bg-lime-400 flex items-center gap-2">
+                Salvar atividade
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
